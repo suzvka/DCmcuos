24687894302ec0d6e8ca77a8d3047fe6c441dd65
云归子 <14542014+tianyuan-x_0@user.noreply.gitee.com>
2025-08-23 12:50:24 +0800

    整理API格式

--- API/API.h
+++ API/API.h
@@ -5,10 +5,13 @@ extern "C" {
 #endif
 #include <stdint.h>
 
-
-
+// 开始运行
 void StartRun();
+
+// 睡眠指定毫秒数
 void sleep(uint32_t ms);
+
+// 获取当前运行频率，单位 Hz
 uint64_t RunningFreq();
 
 //=========================================================
@@ -16,9 +19,6 @@ uint64_t RunningFreq();
 
 // 添加任务
 void AddTask(void(*task)(), uint16_t run_interval_ms = 0);
-
-
-
 
 #ifdef __cplusplus
 }
